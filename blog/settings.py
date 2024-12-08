fcf177999434e662dfa10731523fd31601aca2f6
levah Emmanuel <emmanuelnater@gmail.com>
2024-12-08 14:07:00 +0100

    code commit, adding 'whitenoise.middleware.WhiteNoiseMiddleware',

--- blog/settings.py
+++ blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -156,7 +157,8 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_DIRS = [BASE_DIR/"static"]
     
-   
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+  
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
